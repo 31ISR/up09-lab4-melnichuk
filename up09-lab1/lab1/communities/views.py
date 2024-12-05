47c610c7bd5e4ce947159e0ff54dce1275738591
ST3514 <ST3514@TE.local>
2024-12-05 14:25:49 +0600

    full work

--- up09-lab1/lab1/communities/views.py
+++ up09-lab1/lab1/communities/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Communities
 
 # Create your views here.
 
 def comms_list(req):
-    return render(req, 'communities/comms_list.html')
+    comms = Communities.objects.all().order_by('-date')
+    return render(req, 'communities/comms_list.html', {'comms': comms})
+
+def comm_page(req, slug):
+    comm = Communities.objects.get(slug=slug)
+    return render(req, 'communities/comm_page.html', {'comm': comm})
--- up09-lab1/lab1/posts/views.py
+++ up09-lab1/lab1/posts/views.py
@@ -6,3 +6,8 @@ def posts_list(req):
     posts = Post.objects.all().order_by('-date')
     
     return render(req, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(req, slug):       
+    post = Post.objects.get(slug=slug)  
+
+    return render(req, 'posts/post_page.html', {'post': post})
